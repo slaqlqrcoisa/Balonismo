d5f604e1900e162e1cb250b59de5029f7c24b616
Carlos Eduardo <carlosnt254@gmail.com>
2023-06-07 08:47:23 -0300

    Balonismo 2a parte

--- src/Balao.java
+++ src/Balao.java
@@ -4,5 +4,22 @@ public class Balao {
 
   public String numeroId;
   public String registroReg;
+  public String corBalao;
+  public String tipoBalao;
+  double tamanhoBalao;
+  double  valorBalao;
+  int capacidadeBalao;
+  double pesoBalao;
+
+public String voar(Combustivel gasPropano){
+    return "voando com" + gasPropano;
+}
+
+
+
+
+
+
+
 
 }
